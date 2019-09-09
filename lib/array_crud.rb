524eadae1fc1e812a2a6e7b5bb84e9286dc2aeba
Daniel Joo <danhjoo@gmail.com>
2019-09-09 23:00:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = []
-  array.push(element)
+  p array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
   array = []
-  array.unshift(element)
+  p array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
