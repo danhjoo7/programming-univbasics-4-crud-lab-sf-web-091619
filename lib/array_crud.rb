7d8d25f3feb17ff80f3d7caa4892421e53022215
Daniel Joo <danhjoo@gmail.com>
2019-09-09 23:06:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = []
-  p array.push(element)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -18,12 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   array = []
-  array.pop
+  p array.pop
 end
 
 def remove_element_from_start_of_array(array)
   array = []
-  array.shift 
+  p array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
